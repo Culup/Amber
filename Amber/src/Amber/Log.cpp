ecdaa3b61e9e61ef105a86525b9cf249ad823775
Culup <51217665+Culup@users.noreply.github.com>
2025-04-17 06:54:16 -0400

    Started working on Logging system

---
+++ Amber/src/Amber/Log.cpp
@@ -0,0 +1,14 @@
+#include "Log.h"
+
+namespace Amber {
+
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init()
+	{
+
+	}
+
+}
+
