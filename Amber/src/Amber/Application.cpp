141464ad4ca8c22eb0285e600a545242fe2ee609
Culup <51217665+Culup@users.noreply.github.com>
2025-05-06 07:58:06 -0400

    Introduced Window Events

--- Amber/src/Amber/Application.cpp
+++ Amber/src/Amber/Application.cpp
@@ -1,21 +1,31 @@
 #include "abpch.h"
 #include "Application.h"
 
-#include "Amber/Events/ApplicationEvent.h"
 #include "Amber/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Amber {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		AB_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,5 +36,11 @@ namespace Amber {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
